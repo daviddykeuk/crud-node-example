a559731c162197a51456400ea7ccf8b31430f863
David Dyke <daviddykeuk@gmail.com>
2018-03-17 23:55:20 +1100

    completed performance test scripts

--- index.node.js
+++ index.node.js
@@ -5,7 +5,7 @@ const repo = require("./test/helpers/contacts/mockedRepository");
 
 var app = expressApp(null, repo);
 
-app.listen(process.env.PORT, function () {
-	console.log("Example app listening on port %s", process.env.PORT);
+app.listen(process.env.PORT | 8080, function () {
+	console.log("Example app listening on port %s", process.env.PORT | 8080);
 });
  
--- test/performance/performance_test.sh
+++ test/performance/performance_test.sh
@@ -1,13 +1,14 @@
 # use docker to start up the service with all dependencies
-# docker-compose up -d 
+docker-compose -f $PWD/test/performance/docker-compose.yaml build
+docker-compose -f $PWD/test/performance/docker-compose.yaml up -d 
 
 # run artillery tests to fire multiple requests
-npm install -g artillery
+npm install -g artillery@1.6.0-14
 artillery run $PWD/test/performance/performance.spec.yaml -o performance.results.json
 
 # TODO: inspect the results and decide whether to pass or fail
-node $PWD/test/performance/performance_check.js --median 25 --p99 100
+node $PWD/test/performance/performance_check.js --median 50 --p99 500
 
 # tear down
-# docker-compose down
+docker-compose -f $PWD/test/performance/docker-compose.yaml down
 exit 0
